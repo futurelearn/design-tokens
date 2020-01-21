a57deacbad96abfeb7fddf20043a302086f2597b
Nick Fraenkel <nick.fraenkel@futurelearn.com>
2020-01-21 12:24:41 +0000

    Generate CSS custom properties
    In the previous commit we started generating Sass variables from our
    tokens. It's very easy to also generate CSS custom properties, and this
    demonstrates the advantage to having platform-agnostic design tokens
    that can easily be transformed into other formats.
    
    Although we don't currently use custom properties widely, generating
    them and making them available in a way that ensures they're always in
    sync with their Sass equivalents should make it easier to start
    using them where appropriate.

--- build.js
+++ build.js
@@ -8,6 +8,10 @@ const config = {
         {
           "destination": "_variables.scss",
           "format": "scss/variables"
+        },
+        {
+          "destination": "_custom-properties.scss",
+          "format": "css/variables"
         }
       ]
     }
