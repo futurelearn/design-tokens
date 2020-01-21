6cfbfdc8c58418eb0ab0c700ac71e75c17b968de
Nick Fraenkel <nick.fraenkel@futurelearn.com>
2020-01-21 12:19:23 +0000

    Output colours as Sass variables
    `color.json` contains all the colours that are available via our colour
    Sass functions (`colour` and `off-brand-colour` in the main FL repo).
    
    Let's output these as Sass variables, suitable for importing into
    our stylesheets.

--- build.js
+++ build.js
@@ -1,4 +1,18 @@
-const config = {};
+const config = {
+  "source": ["properties/**/*.json"],
+  "platforms": {
+    "scss": {
+      "transformGroup": "scss",
+      "buildPath": "build/scss/",
+      "files": [
+        {
+          "destination": "_variables.scss",
+          "format": "scss/variables"
+        }
+      ]
+    }
+  }
+};
 
 const StyleDictionary = require('style-dictionary').extend(config);
 StyleDictionary.buildAllPlatforms();
