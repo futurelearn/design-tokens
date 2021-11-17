999e318354149e2a45b0996d2854e35047182b7c
Nick Fraenkel <nick.fraenkel@futurelearn.com>
2021-11-17 09:51:51 +0000

    Consolidate "type-scale" breakpoints with standard breakpoints
    Trello: https://trello.com/c/lj4H3kYO
    
    We have a couple of "type-scale" breakpoints that define where our font
    changes size. Slightly awkwardly, these don't line up with our main
    breakpoints, though they're quite close to them.
    
    This has the effect that there's a couple of "ghost breakpoints" between
    648px and 680px, and again between 1600px and 1695px, in which the
    layout can be unexpectedly different (the font size change at 680px in
    particular can cause quite significant layout changes).
    
    If we can reconcile these breakpoints with each other we can remove these
    "ghost" breakpoints, and simplify things a bit.
    
    I recently opened [an RFC][1] proposing that we reconcile these
    breakpoints in order to remove these unpredictable ghost breakpoints and
    simplify things a bit.
    
    Let's do this now, by:
    
    - Increasing the "medium" breakpoint to match the "typeScaleMedium"
      breakpoint at 680px (this seems safer than trying to increase the font
      size at a smaller screen size)
    
    - Reducing the "typeScaleLarge" breakpoint to match the "xxlarge"
      breakpoint at 1600px (since this is already wider than most page
      content containers, reducing it seems less likely to cause problems
      in this case
    
    (See the RFC if you'd like even more detail).
    
    This leaves us temporarily with some identical breakpoints. We can
    subsequently tidy this up by replacing all references to the typescale
    breakpoints in our code with references to their standard breakpoint
    equivalent. Once that's done, we should be able to remove the typescale
    breakpoint design tokens altogether.
    
    [1]: https://docs.google.com/document/d/1OwF96r9g1rE28eLeHU4Gcufmp4jwMbAaJi5PE79NfxA/edit?usp=sharing

--- build/js/breakpoints.js
+++ build/js/breakpoints.js
@@ -1,7 +1,7 @@
 module.exports.typeScaleMedium = 680;
-module.exports.typeScaleLarge = 1695;
+module.exports.typeScaleLarge = 1600;
 module.exports.small = 480;
-module.exports.medium = 648;
+module.exports.medium = 680;
 module.exports.large = 900;
 module.exports.xlarge = 1280;
 module.exports.xxlarge = 1600;
